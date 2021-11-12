de452d4f50fd46f8ba346a3280e245171fa020f1
DeepSmashProject <deepsmash0@gmail.com>
2021-11-13 00:12:59 +0900

    update Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,7 @@
+docker run --privileged -it --rm --gpus all \
+  -p 8081:8081 \
+  -e RESOLUTION=1280x720 \
+  -e VNCPASS=pass \
+  -v "/Users/ruihirano/MyProjects/DeepSmashProject/:/workspace" \
+  -v "/Users/ruihirano/MyProjects/DeepSmashProject/data/keys:/root/.local/share/yuzu/keys" \
+  --name yuzu_emu yuzu_emu
