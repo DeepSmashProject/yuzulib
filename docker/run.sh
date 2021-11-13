1e4abe4d6b6f9d9abab63aa4131f24dd360c7687
DeepSmashProject <deepsmash0@gmail.com>
2021-11-13 09:34:12 +0900

    update Dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 docker run --privileged -it --rm --gpus all \
   -p 8081:8081 \
-  -e RESOLUTION=1280x720 \
+  -e RESOLUTION=1280x800 \
   -e VNCPASS=pass \
   -v "/Users/ruihirano/MyProjects/DeepSmashProject/:/workspace" \
   -v "/Users/ruihirano/MyProjects/DeepSmashProject/data/keys:/root/.local/share/yuzu/keys" \
